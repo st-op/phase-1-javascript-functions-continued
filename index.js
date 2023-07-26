5d6b380eaad7dc37981e2e27d4157b909c01525b
Stella Johansson <stellajoh9@gmail.com>
2023-07-26 12:41:13 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun('bathe my dog')
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork('work from home')
+
+function wrapAdjective(symbol = "*") {
+    return function superCool(description = "a hard worker", phrase = "You are") {
+      return `${phrase} ${symbol}${description}${symbol}!`;
+    };
+  }
+wrapAdjective('||')("a dedicated programmer");
+
+// You are *a hard worker* 
+// you are ||a dedicated programmer||
